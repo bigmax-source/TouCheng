2822d7fde18e79823768502c7f814829b6cfdcb2
Polonews <bigmax@tmail.ilc.edu.tw>
2026-09-06 15:26:40 +0800

    Add files via upload

--- assets/data/site-meta.js
+++ assets/data/site-meta.js
@@ -1,12 +1,12 @@
 window.SITE_META = {
   version: "1.85",
-  updatedAt: "2026-08-31",
+  updatedAt: "2026-09-06",
   projectName: "頭城地方記憶典藏",
-  historyCount: 497,
+  historyCount: 498,
   coverage: "1731–2026",
   sourceTypeCount: 5,
   peopleSpaceCount: 21,
-  storyCount: 41,
+  storyCount: 44,
   curationCount: 3,
   placeCount: 34
 };
